1c42e79e462628e934ebeb3862e70dfe687f2120
CN045696 <CN045696@W1571417.northamerica.cerner.net>
2017-08-02 23:10:11 +0530

    Ascii Commit

---
+++ src/Ascii.java
@@ -0,0 +1,16 @@
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       for (int i=60; i<=90; i++)
+       
+	
+	{
+		
+		System.out.printf(" %d : %c \n ",i,i);
+	}
+
+}
+	
+}
